c7eb941e986d676e2cd9838e9d27641bf2c622af
Victoria Sierra <sierravicc@gmail.com>
2022-05-19 14:50:01 -0500

    segundo commit

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  //styleUrls: ['./main-page.component.css']
+})
+
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  constructor () {}
+}
+ 
--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -13,3 +13,4 @@ export class ListadoComponent {
     this.heroeBorrado = this.heroes.shift() || '';
   }
 }
+ 
